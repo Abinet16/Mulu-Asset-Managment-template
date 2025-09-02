a4b52a0602e3664c48729caf96bb14b78494f990
Abinet16 <59099776+Abinet16@users.noreply.github.com>
2025-09-02 16:03:20 +0300

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://mulu-asset.netlify.app",
     credentials: true, // Add this if using cookies/auth
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
